be0e31e3855be6b2c427d372829803bdcb337fc2
MK <parisburner6969@gmail.com>
2024-04-17 00:20:42 +0300

    Added troposphere and changed beanstalk setup to a working one

--- beanstalk.py
+++ beanstalk.py
@@ -1,51 +1,24 @@
-#!/usr/bin/env python
-from aws_cdk import (
-    aws_elasticbeanstalk as elasticbeanstalk,
-    aws_ec2 as ec2,
-    core,
-)
+from troposphere import Template, elasticbeanstalk as eb
 
+template = Template()
 
-class ElasticBeanstalkStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
+eb_application = template.add_resource(eb.Application(
+    "MyApplication",
+    Description="My Elastic Beanstalk Application"
+))
 
-        # Create a VPC for the Elastic Beanstalk environment
-        vpc = ec2.Vpc(
-            self, "MyVpc",
-            max_azs=2
+eb_environment = template.add_resource(eb.Environment(
+    "MyEnvironment",
+    ApplicationName="MyApplication",  # Directly provide the application name
+    Description="My Elastic Beanstalk Environment",
+    SolutionStackName="64bit Amazon Linux 2023 v4.0.10 running Python 3.11",
+    OptionSettings=[
+        eb.OptionSetting(
+            Namespace="aws:autoscaling:launchconfiguration",
+            OptionName="InstanceType",
+            Value="t2.nano"
         )
+    ]
+))
 
-        # Create an Elastic Beanstalk application
-        application = elasticbeanstalk.CfnApplication(
-            self, "MyApplication",
-            application_name="MyApplication"
-        )
-
-        # Create an Elastic Beanstalk environment
-        environment = elasticbeanstalk.CfnEnvironment(
-            self, "MyEnvironment",
-            application_name=application.application_name,
-            solution_stack_name="64bit Amazon Linux 2 v5.4.5 running Python 3.8",
-            cname_prefix="my-env",
-            tier="WebServer",
-            option_settings=[
-                {
-                    "namespace": "aws:autoscaling:asg",
-                    "option_name": "MinSize",
-                    "value": "2",
-                },
-                {
-                    "namespace": "aws:autoscaling:asg",
-                    "option_name": "MaxSize",
-                    "value": "6",
-                },
-            ],
-            vpc_id=vpc.vpc_id,
-            instances_role=f"ElasticBeanstalkEC2Role-{core.Aws.ACCOUNT_ID}",
-        )
-
-
-app = core.App()
-ElasticBeanstalkStack(app, "ElasticBeanstalkStack")
-app.synth()
+print(template.to_yaml())
