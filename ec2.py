c29df1693f8afacff033d3c1bc4bd19914081a1d
MK <parisburner6969@gmail.com>
2024-04-18 23:10:07 +0300

    New CentOS machine for portfolio website

--- ec2.py
+++ ec2.py
@@ -13,14 +13,34 @@ key_name_param = template.add_parameter(Parameter(
 
 # Define EC2 instance
 ec2_instance = ec2.Instance(
-    "MyEC2Instance",
-    ImageId="ami-0f7204385566b32d0",  # Amazon Linux 2 AMI ID
+    "PortfolioHost",
+    ImageId="ami-0afcbcee3dfbce929",  # CentOS AMI ID
     InstanceType="t3.nano",
     KeyName=Ref(key_name_param),
 )
 
 # Add the EC2 instance to the template
 template.add_resource(ec2_instance)
+
+# Define security group
+ssh_sg = ec2.SecurityGroup(
+    "SSHSecurityGroup",
+    GroupDescription="Allow SSH access from my Turkey IP",
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="185.117.123.142/32",  # Your IP address
+        ),
+    ],
+)
+
+# Add the security group to the template
+template.add_resource(ssh_sg)
+
+# Attach the security group to the EC2 instance
+ec2_instance.SecurityGroups = [Ref(ssh_sg)]
 
 # Print the CloudFormation template
 print(template.to_yaml())
