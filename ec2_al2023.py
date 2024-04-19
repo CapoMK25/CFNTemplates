69131a44743260202e1e00f3760b7b60f17031ce
MK <parisburner6969@gmail.com>
2024-04-19 15:24:04 +0300

    Added template for where to live al2023

---
+++ ec2_al2023.py
@@ -0,0 +1,53 @@
+from troposphere import Template, Ref, Parameter, ec2
+
+# Create a template
+template = Template()
+
+# Define parameters
+key_name_param = template.add_parameter(Parameter(
+    "KeyName",
+    Type="String",
+    Description="Name of an existing EC2 KeyPair to enable SSH access to the instance",
+    Default="main",
+))
+
+# Define EC2 instance
+ec2_instance = ec2.Instance(
+    "WhereToLiveHost",
+    ImageId="ami-0f673487d7e5f89ca",  # Amazon Linux 2023 AMI ID
+    InstanceType="t3.nano",
+    KeyName=Ref(key_name_param),
+)
+
+# Add the EC2 instance to the template
+template.add_resource(ec2_instance)
+
+# Define security group (Amazon Linux 2023)
+linux_sg = ec2.SecurityGroup(
+    "AmazonLinuxSecurityGroup",
+    GroupDescription="Allow SSH access from my Turkey IP",
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="185.117.123.142/32",  # Your IP address
+        ),
+        # Add HTTP ingress rule to Amazon Linux security group
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=80,
+            ToPort=80,
+            CidrIp="185.117.123.142/32",  # My IP
+        ),
+    ],
+)
+
+# Add the security group to the template
+template.add_resource(linux_sg)
+
+# Attach the security group to the EC2 instance
+ec2_instance.SecurityGroups = [Ref(linux_sg)]
+
+# Print the CloudFormation template
+print(template.to_yaml())
